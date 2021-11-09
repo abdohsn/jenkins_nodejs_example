84fdc2b0ecc9a2d53358b596952b89d4c6595964
Abdelrahaman-Hassan <abdohsn39@yahoo.com>
2021-11-09 04:13:41 +0200

    Feature: adding and using dotenv package

--- dockerfile
+++ dockerfile
@@ -1,5 +1,9 @@
-FROM node:12
+FROM node:12.16.1-alpine3.9
+
 COPY nodeapp /nodeapp
+
 WORKDIR /nodeapp
+
 RUN npm install
+
 CMD ["node", "/nodeapp/app.js"]
--- nodeapp/app.js
+++ nodeapp/app.js
@@ -1,8 +1,12 @@
+const ENV = require("./env");
+
+const name = ENV.getVar("NAME")
+
 const express = require('express')
 const app = express()
 const port = 3000
 app.get('/', (req, res) => {
-    res.send('Hello World from ITI!')
+    res.send(`Hello World from ${name}!`)
   })
   
   app.listen(port, () => {
